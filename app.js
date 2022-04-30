005931043a618a2ab253f3778757df5b9ca8b519
HPsisodia <kunwarhardik1999@gmail.com>
2022-04-30 15:30:31 +0530

    addedd procfile

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ const moment = require('moment');
 
 require("dotenv").config();
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const client = require('twilio')(accountSid, authToken);
 
 const gPickupModel = require('./models/garbagepickup');
 const customerModel = require('./models/customer');
